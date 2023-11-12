a2ef9d9a179e2b15802a45c1476d4685779f1225
isobel <isobel@isobels-MacBook-Air.local>
2023-11-12 11:49:55 +0000

    Moving to google collab

--- FileHelper.py
+++ FileHelper.py
@@ -1,19 +1,32 @@
 import numpy as np
+import uproot
 
 from tensorflow.keras.utils import to_categorical
 
-def readTree(branches, dimensions, nClasses):
+def readTreeOLD(branches, dimensions, nClasses, nEntries):
 
+    print('Reading tree... this may take a while...')
+    
     # This is dumb... I have to do this because of empty entries in the vectors - SAD
 
+    print()
+    
     startIndex = -1
 
     for i in range(0, len(branches)) :
+        
+        #print("A: Index: ", str(i))
+        
         if (len(branches['StartGridU'][i]) == 0)|(len(branches['StartGridV'][i]) == 0)|(len(branches['StartGridW'][i]) == 0) :
+            continue
+            
+        if (len(branches['TruePDG'][i]) == 0):
             continue
         
         startIndex = i
         break
+        
+    print("Start Index: ", str(startIndex))
     
     startGridU = np.asarray(branches['StartGridU'][startIndex]).reshape(1, dimensions, dimensions, 1)
     startGridV = np.asarray(branches['StartGridV'][startIndex]).reshape(1, dimensions, dimensions, 1)
@@ -23,8 +36,29 @@ def readTree(branches, dimensions, nClasses):
     endGridW = np.asarray(branches['EndGridW'][startIndex]).reshape(1, dimensions, dimensions, 1)
     particlePDG = np.asarray(branches['TruePDG'][startIndex]).reshape(1, 1)
 
+    count = 1
+    
     for i in range((startIndex + 1), len(branches)) :
+        
+        #print("B: Index: ", str(i))
+        
+        #print('StartGridU: ', str(len(branches['StartGridU'][i])))
+        #print('StartGridV: ', str(len(branches['StartGridV'][i])))
+        #print('StartGridW: ', str(len(branches['StartGridW'][i])))
+        #print('TruePDG: ', str(len(branches['TruePDG'][i])))
+        #print('---------------------------')
+        
         if (len(branches['StartGridU'][i]) == 0)|(len(branches['StartGridV'][i]) == 0)|(len(branches['StartGridW'][i]) == 0) :
+            continue
+            
+        if (len(branches['TruePDG'][i]) == 0):
+            continue
+            
+        # First, completeness and nHit
+        completeness = branches['Completeness'][i]
+        purity = branches['Purity'][i]
+
+        if (completeness < 0.5) | (purity < 0.5) :
             continue
     
         thisStartGridU = np.asarray(branches['StartGridU'][i]).reshape(1, dimensions, dimensions, 1)
@@ -45,8 +79,24 @@ def readTree(branches, dimensions, nClasses):
         
         thisParticlePDG = np.asarray(branches['TruePDG'][i]).reshape(1, 1)
         particlePDG = np.concatenate((particlePDG, thisParticlePDG))
+        
+        count += 1
+        
+        #print('Count: ', str(count))
+        
+        if (count >= nEntries):
+            break
+            
+    print('Read: ', str(count), ' entries from tree')
 
 
+    print('We have...')
+    print('nMuons: ', np.count_nonzero(abs(particlePDG) == 13))
+    print('nProtons: ', np.count_nonzero(abs(particlePDG) == 2212))    
+    print('nPions: ', np.count_nonzero(abs(particlePDG) == 211))     
+    print('nElectrons: ', np.count_nonzero(abs(particlePDG) == 11))     
+    print('nPhotons: ', np.count_nonzero(abs(particlePDG) == 22))     
+    print('nOther: ', np.count_nonzero((abs(particlePDG) != 13) & (abs(particlePDG) != 2212) & (abs(particlePDG) != 221) & (abs(particlePDG) != 11) &  (abs(particlePDG) != 22)))
     # Need to alter the format & shape of particle PDG
 
     # muons = 0, protons = 1, pions = 2, electrons = 3, photons = 4, other = 5
@@ -59,4 +109,82 @@ def readTree(branches, dimensions, nClasses):
 
     y = to_categorical(particlePDG, nClasses)
 
-    return startGridU, startGridV, startGridW, endGridU, endGridV, endGridW, particlePDG
+    return startGridU, startGridV, startGridW, endGridU, endGridV, endGridW, y
+
+############################################################################
+
+def readTree(fileNames, dimensions, nClasses) :
+
+    # Lists
+    startGridU = []
+    startGridV = []
+    startGridW = []
+    
+    endGridU = []
+    endGridV = []
+    endGridW = []
+    particlePDG = []
+    
+    nEntries = 0
+    
+    for fileName in fileNames :
+        print('Reading tree: ', str(fileName),', This may take a while...')
+    
+        treeFile = uproot.open(fileName)
+        tree = treeFile['ivyTrain/ivysaur']
+        branches = tree.arrays()
+        
+        nEntries += len(branches)
+    
+        startGridU.extend(branches['StartGridU'])
+        startGridV.extend(branches['StartGridV'])
+        startGridW.extend(branches['StartGridW'])
+        
+        endGridU.extend(branches['EndGridU'])
+        endGridV.extend(branches['EndGridV'])
+        endGridW.extend(branches['EndGridW'])
+        
+        particlePDG.extend(branches['TruePDG'])
+        
+        
+    # Now turn things into numpy arrays
+    startGridU = np.array(startGridU)
+    startGridV = np.array(startGridV)    
+    startGridW = np.array(startGridW)    
+    
+    endGridU = np.array(endGridU)
+    endGridV = np.array(endGridV)
+    endGridW = np.array(endGridW)
+    
+    particlePDG = np.array(particlePDG)
+    
+    startGridU.reshape(nEntries, dimensions, dimensions, 1)
+    startGridV.reshape(nEntries, dimensions, dimensions, 1)
+    startGridW.reshape(nEntries, dimensions, dimensions, 1)
+    endGridU.reshape(nEntries, dimensions, dimensions, 1)
+    endGridV.reshape(nEntries, dimensions, dimensions, 1)
+    endGridW.reshape(nEntries, dimensions, dimensions, 1)
+    particlePDG.reshape(nEntries, 1)
+    
+    print('We have ', str(nEntries), ' PFParticles overall!')
+    print('nMuons: ', np.count_nonzero(abs(particlePDG) == 13))
+    print('nProtons: ', np.count_nonzero(abs(particlePDG) == 2212))    
+    print('nPions: ', np.count_nonzero(abs(particlePDG) == 211))     
+    print('nElectrons: ', np.count_nonzero(abs(particlePDG) == 11))     
+    print('nPhotons: ', np.count_nonzero(abs(particlePDG) == 22))     
+    print('nOther: ', np.count_nonzero((abs(particlePDG) != 13) & (abs(particlePDG) != 2212) & (abs(particlePDG) != 221) & (abs(particlePDG) != 11) &  (abs(particlePDG) != 22)))
+   
+    # muons = 0, protons = 1, pions = 2, electrons = 3, photons = 4, other = 5
+    particlePDG[abs(particlePDG) == 13] = 0
+    particlePDG[abs(particlePDG) == 2212] = 1
+    particlePDG[abs(particlePDG) == 211] = 2
+    particlePDG[abs(particlePDG) == 11] = 3
+    particlePDG[abs(particlePDG) == 22] = 4
+    particlePDG[(abs(particlePDG) != 0) & (abs(particlePDG) != 1) & (abs(particlePDG) != 2) & (abs(particlePDG) != 3) &  (abs(particlePDG) != 4)] = 5
+
+    y = to_categorical(particlePDG, nClasses)
+
+    return startGridU, startGridV, startGridW, endGridU, endGridV, endGridW, y
+
+
+
--- IvysaurusModel.py
+++ IvysaurusModel.py
@@ -2,29 +2,93 @@ from tensorflow import keras
 from tensorflow.keras import layers
 
 # start/endGrid.shape = (n, dimensions, dimensions, 1)
-def IvysaurusIChooseYou(dimensions, nclasses):
-    
-    # TODO: also need to do a normalisation - I think i've done that with energies
+def IvysaurusIChooseYou(dimensions, nclasses, meanStartU, varStartU, meanStartV, varStartV, meanStartW, varStartW, meanEndU, varEndU, meanEndV, varEndV, meanEndW, varEndW):
     
     ################################
     # U View
     ################################
-    # Start U branch
+    startInputsU = keras.layers.Normalization(-1, mean=meanStartU, variance=varStartU)
     startInputsU = keras.Input(shape=(dimensions, dimensions, 1))
-    startBranchU = layers.Conv2D(filters=32, kernel_size=3, activation="relu")(startInputsU)
-    startBranchU = layers.Flatten()(startBranchU)
-
-    # End U branch
+    endInputsU = keras.layers.Normalization(-1, mean=meanEndU, variance=varEndU)
     endInputsU = keras.Input(shape=(dimensions, dimensions, 1))
-    endBranchU = layers.Conv2D(filters=32, kernel_size=3, activation="relu")(endInputsU)
-    endBranchU = layers.Flatten()(endBranchU)
+    branchU = CreateViewBranch(dimensions, startInputsU, endInputsU)
     
-    # Now combine the U branches
-    combinedU = keras.layers.Concatenate()([startBranchU, endBranchU])
+    ################################
+    # V View
+    ################################
+    startInputsV = keras.layers.Normalization(-1, mean=meanStartV, variance=varStartV)
+    startInputsV = keras.Input(shape=(dimensions, dimensions, 1))
+    endInputsV = keras.layers.Normalization(-1, mean=meanEndV, variance=varEndV)
+    endInputsV = keras.Input(shape=(dimensions, dimensions, 1))
+    branchV = CreateViewBranch(dimensions, startInputsV, endInputsV)    
+    
+    ################################
+    # W View
+    ################################
+    startInputsW = keras.layers.Normalization(-1, mean=meanStartW, variance=varStartW)
+    startInputsW = keras.Input(shape=(dimensions, dimensions, 1))
+    endInputsW = keras.layers.Normalization(-1, mean=meanEndW, variance=varEndW)
+    endInputsW = keras.Input(shape=(dimensions, dimensions, 1))
+    branchW = CreateViewBranch(dimensions, startInputsW, endInputsW)
+    
+    # Now combine the U, V and W branches
+    combined = keras.layers.Concatenate()([branchU, branchV, branchW])
+    
+    # FC layer??
+    combined = keras.layers.Dense(128, activation="relu")(combined)
     
     # Now classify the image
-    outputs = layers.Dense(nclasses, activation="softmax")(combinedU)
+    outputs = keras.layers.Dense(nclasses, activation="softmax")(combined)
     
-    model = keras.Model(inputs=[startInputsU, endInputsU], outputs=outputs)
+    model = keras.Model(inputs=[startInputsU, endInputsU, startInputsV, endInputsV, startInputsW, endInputsW], outputs=outputs)
     
     return model
+
+
+def CreateViewBranch(dimensions, startInputs, endInputs):
+    ################################
+    # Start branch
+    ################################
+    # 1
+    startBranch = layers.Conv2D(filters=32, kernel_size=3, activation="relu", padding="same")(startInputs)
+    startBranch = layers.MaxPooling2D(pool_size=2)(startBranch)
+    # 2
+    startBranch = layers.Conv2D(filters=64, kernel_size=3, activation="relu", padding="same")(startBranch)
+    startBranch = layers.MaxPooling2D(pool_size=2)(startBranch)
+    # 3
+    startBranch = layers.Conv2D(filters=128, kernel_size=3, activation="relu", padding="same")(startBranch)
+    #startBranch = layers.MaxPooling2D(pool_size=2)(startBranch)
+    # 4
+    startBranch = layers.Conv2D(filters=256, kernel_size=3, activation="relu", padding="same")(startBranch)
+    #startBranch = layers.MaxPooling2D(pool_size=2)(startBranch)
+    # 5
+    #startBranch = layers.Conv2D(filters=256, kernel_size=3, activation="relu", padding="same")(startBranch)
+    #startBranch = layers.MaxPooling2D(pool_size=2)(startBranch) 
+    startBranch = layers.Flatten()(startBranch)
+
+    ################################
+    # End branch
+    ################################
+    # 1
+    endBranch = layers.Conv2D(filters=32, kernel_size=3, activation="relu", padding="same")(endInputs)
+    endBranch = layers.MaxPooling2D(pool_size=2)(endBranch)
+    # 2
+    endBranch = layers.Conv2D(filters=64, kernel_size=3, activation="relu", padding="same")(endBranch)
+    endBranch = layers.MaxPooling2D(pool_size=2)(endBranch)    
+    # 3
+    endBranch = layers.Conv2D(filters=128, kernel_size=3, activation="relu", padding="same")(endBranch)
+    #endBranch = layers.MaxPooling2D(pool_size=2)(endBranch)    
+    # 4
+    endBranch = layers.Conv2D(filters=256, kernel_size=3, activation="relu", padding="same")(endBranch)
+    #endBranch = layers.MaxPooling2D(pool_size=2)(endBranch)    
+    # 5
+    #endBranch = layers.Conv2D(filters=256, kernel_size=3, activation="relu", padding="same")(endBranch)
+    #endBranch = layers.MaxPooling2D(pool_size=2)(endBranch)
+    endBranch = layers.Flatten()(endBranch)
+    
+    ################################
+    # Now combine the start/end branches
+    ################################
+    combined = keras.layers.Concatenate()([startBranch, endBranch])
+    
+    return combined
